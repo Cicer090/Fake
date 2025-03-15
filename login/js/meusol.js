d76888145d6624ddf31ca301855ef32c4ce04bff
Cicero romao <149887261+Cicer090@users.noreply.github.com>
2025-03-15 18:31:20 -0300

    Atualizar o meusol.js

--- login/js/meusol.js
+++ login/js/meusol.js
@@ -204,7 +204,7 @@ function validate() {
     var username = document.getElementById("username").value;
     var password = document.getElementById("password").value;
 
-    if (username == "Neto" && password == "15032025") {
+    if (username == "Amor" && password == "15032025") {
         window.location = "final.html";
         return false;
     }
@@ -215,5 +215,5 @@ function validate() {
 }
 
 function alerta() {
-    swal("Desculpa!", "Não posso aceitar, você não parece ser Claudia Medeiros! :(")
+    swal("Desculpa!", "Não posso aceitar, você não parece ser Meu Amor! :(")
 }
